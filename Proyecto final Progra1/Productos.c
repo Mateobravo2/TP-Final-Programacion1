01b23fae7384faa3afd1191e3d10ccc888bd169a
Mateo Bravo <mateobravo552@gmail.com>
2026-06-21 20:13:11 -0300

    Update Productos.c

--- Proyecto final Progra1/Productos.c
+++ Proyecto final Progra1/Productos.c
@@ -5,21 +5,18 @@
 ///ALTA PRODUCTO
 void cargarStProducto(char archivoProducto[])
 {
+    stProducto prod;
     FILE* archi = fopen(archivoProducto, "r+b");
     if(archi!=NULL)
     {
-        stProducto prod;
         int i=0;
         char control='s';
-
         while(fread(&prod, sizeof(stProducto), 1, archi) > 0)
         {
             i++;
         }
-
         while(control =='s')
         {
-
             prod= cargaProducto(i+1);
 
             fwrite(&prod, sizeof(stProducto), 1, archi);
@@ -27,7 +24,6 @@ void cargarStProducto(char archivoProducto[])
             printf("desea seguir cargando datos? s/n\n");
 
             scanf(" %c", &control);
-
         }
     }
 }
@@ -36,12 +32,9 @@ void cargarStProducto(char archivoProducto[])
 stProducto cargaProducto(int i)
 {
     stProducto aux;
-
     aux.id= i;
-
     printf("ingrese el nombre del producto\n");
     scanf(" %s", &aux.nombre);
-
     printf("ingrese el precio del producto\n");
     scanf(" %f", &aux.precio);
     if(aux.precio<2000)
@@ -52,30 +45,25 @@ stProducto cargaProducto(int i)
     {
         aux.economico=0;
     }
-
     printf("ingrese la cantidad de productos\n");
     scanf(" %i", &aux.stock);
-
     aux.activo=1;
-
     return aux;
 }
 
 ///CONSULTA PRODUCTOS
 void mostrarProducto(char archivoProducto[])
 {
+    stProducto prod;
     FILE* archi= fopen(archivoProducto, "rb");
     if(archi!=NULL)
     {
-        stProducto prod;
-
         while(fread(&prod, sizeof(stProducto), 1, archi)>0)
         {
             if(prod.activo==1)
             {
                 mostrarP(prod);
             }
-
         }
         fclose(archi);
     }
@@ -85,11 +73,12 @@ void mostrarProducto(char archivoProducto[])
 void mostrarP(stProducto prod)
 {
     printf("\n");
-    printf("---------------\n");
+    printf("_________________________\n");
     printf("id de producto %i\n", prod.id);
     printf("%s\n", prod.nombre);
     printf(" precio $%.2f\n", prod.precio);
     printf("unidades %i\n", prod.stock);
+    printf("-------------------------\n");
 }
 
 
@@ -98,9 +87,7 @@ void buscarProducto(char archivoProducto[])
     FILE* archi= fopen(archivoProducto, "r+b");
     if(archi!=NULL)
     {
-
         int controlP=0;
-
         do
         {
             printf("-------MENU-------\n");
@@ -110,35 +97,29 @@ void buscarProducto(char archivoProducto[])
             printf("2. buscar producto por id\n");
             printf("0. si desea salir\n");
             scanf(" %i", &controlP);
-
             switch(controlP)
             {
             case 1:
                 buscarPorNombre(archi);
                 break;
-
             case 2:
                 buscarPorId(archi);
                 break;
             }
-
         }
         while(controlP==0);
         fclose(archi);
     }
-
 }
 
 
 void buscarPorNombre(FILE* archi)
 {
     stProducto prod;
-
     int banderaP=0;
     char nombreAux[20];
     char banderaN='s';
     char banderaNo='s';
-
     while(banderaN=='s')
     {
         printf("ingrese el nombre del producto\n");
@@ -155,12 +136,9 @@ void buscarPorNombre(FILE* archi)
                 if(banderaNo=='s')
                 {
                     prod = menuProdElegido(prod);
-
                     fseek(archi, sizeof(stProducto)*(-1), SEEK_CUR);
                     fwrite(&prod, sizeof(stProducto),1, archi);
-
                     banderaP=1;
-
                 }
                 else
                 {
@@ -174,7 +152,6 @@ void buscarPorNombre(FILE* archi)
         }
         printf("desea buscar otro producto? s/n\n");
         scanf(" %c", &banderaN);
-
         fseek(archi, 0, SEEK_SET);
         banderaP=0;
     }
@@ -183,18 +160,14 @@ void buscarPorNombre(FILE* archi)
 void buscarPorId(FILE* archi)
 {
     stProducto prod;
-
     char banderaP = 's';
     int idAux;
     int banderap=0;
     char banderaId='s';
-
     while(banderaId=='s')
     {
         printf("ingrese el id del producto\n");
         scanf(" %i", &idAux);
-
-
         while(fread(&prod, sizeof(stProducto), 1, archi)>0 && banderap==0 )
         {
             if (idAux==prod.id && prod.activo==1)
@@ -206,14 +179,10 @@ void buscarPorId(FILE* archi)
 
                 if(banderaP=='s')
                 {
-
                     prod = menuProdElegido(prod);
-
                     fseek(archi, sizeof(stProducto)*(-1), SEEK_CUR);
                     fwrite(&prod, sizeof(stProducto),1, archi);
-
                     banderap=1;
-
                 }
                 else
                 {
@@ -225,13 +194,10 @@ void buscarPorId(FILE* archi)
         {
             printf("error el producto buscado no existe\n");
         }
-
         printf("desea buscar otro producto? s/n\n");
         scanf(" %c", &banderaId);
-
         fseek(archi, 0, SEEK_SET);
         banderap=0;
-
     }
 }
     //MODIFICACION Y BAJA
@@ -239,9 +205,7 @@ void buscarPorId(FILE* archi)
 stProducto menuProdElegido(stProducto prod)
 {
     int opp=0;
-
     stProducto aux;
-
     do
     {
         printf("------MENU PRODUCTO %s-----\n", prod.nombre);
@@ -250,21 +214,16 @@ stProducto menuProdElegido(stProducto prod)
         printf("3.  modificar stock\n");
         printf("4. eliminar producto\n");
         printf("0. salir\n");
-
         scanf("%i", &opp);
-
         switch(opp)
         {
-
         case 1:
             printf("escriba el nuevo nombre\n");
             scanf(" %s", &prod.nombre);
             break;
-
         case 2:
             printf("que precio quiere seleccionar para %s\n", prod.nombre);
             scanf("%f", aux.precio);
-
             while(aux.precio < 0)
             {
                 printf("ERROR, el precio debe ser de 0 o mas\n");
@@ -296,9 +255,7 @@ stProducto menuProdElegido(stProducto prod)
             prod.activo=0;
             printf("el archivo a sido eliminado.\n");
             break;
-
         }
-
     }
     while(opp==0);
     return prod;
